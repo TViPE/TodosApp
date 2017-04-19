9f62cfcc4bc634fe48117145b1de18cc83dc7cc4
TRI VI <trivi2710@gmail.com>
2017-04-19 15:08:02 -0400

    Todo routing
    Feat: Todo routing
    
    A routing allows us to easily filter the list of items that are active
    as well as those which have been completed.

---
+++ js/routers/router.js
@@ -0,0 +1,23 @@
+// Todo Router
+
+var app = app || {};
+
+var Workspace = Backbone.Router.extend({
+	routes: {
+		'*filter' : 'setFilter'
+	},
+
+	setFilter: function(param) {
+		// Set the current filter to be used
+		if (param) {
+			param = param.trim();
+		}
+		app.TodoFilter = param || '';
+
+		// Trigger a collection fitler event, causing hiding, unhiding
+		app.Todos.trigger('filter');
+	}
+});
+
+app.TodoRouter = new Workspace();
+Backbone.history.start();
